74aca76ac747fde645ff9318964eafabe8c409f2
DouaeBakkali269 <P138324714@um5.ac.ma>
2025-03-03 16:56:44 +0100

    xEercice 2 : Obtenir une occurence à partir de son identifiant

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -12,30 +12,75 @@ public class Main {
                 .addAnnotatedClass(Entreprise.class)
                 .buildSessionFactory();
 
-        // Create a Session
+        // Insert multiple entreprises (Exercise 1)
+        insertMultipleEntreprises(factory);
+
+        // Retrieve an Entreprise by ID (Exercise 2)
+        Entreprise entreprise = getEntrepriseById(factory, 1);
+        if (entreprise != null) {
+            System.out.println("Retrieved Entreprise: " + entreprise.getNomEnt());
+        } else {
+            System.out.println("Entreprise not found.");
+        }
+
+        // Close the SessionFactory
+        factory.close();
+    }
+
+    // Method to insert multiple entreprises (Exercise 1)
+    public static void insertMultipleEntreprises(SessionFactory factory) {
         Session session = factory.getCurrentSession();
 
         try {
-            // Create a new Entreprise object
-            // Add a new entreprise
-            Entreprise entreprise = new Entreprise();
-            entreprise.setNomEnt("Tech Corp");
-            entreprise.setListeActivities("Software Development");
-            entreprise.setNbEmployee(100);
-
             // Start a transaction
             session.beginTransaction();
 
-            // Save the Entreprise object
-            session.save(entreprise);
+            // Create and save multiple entreprises
+            Entreprise entreprise1 = new Entreprise();
+            entreprise1.setNomEnt("Tech Corp");
+            entreprise1.setListeActivities("Software Development");
+            entreprise1.setNbEmployee(100);
+            session.save(entreprise1);
+
+            Entreprise entreprise2 = new Entreprise();
+            entreprise2.setNomEnt("Health Inc");
+            entreprise2.setListeActivities("Healthcare");
+            entreprise2.setNbEmployee(200);
+            session.save(entreprise2);
+
+            Entreprise entreprise3 = new Entreprise();
+            entreprise3.setNomEnt("Edu Solutions");
+            entreprise3.setListeActivities("Education");
+            entreprise3.setNbEmployee(150);
+            session.save(entreprise3);
 
             // Commit the transaction
             session.getTransaction().commit();
 
-            // we print the id of the added entreprise
-            System.out.println("Entreprise saved with ID: " + entreprise.getIdEntreprise());
-        } finally {
-            factory.close();
+            System.out.println("Multiple entreprises inserted successfully.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to retrieve an Entreprise by ID (Exercise 2)
+    public static Entreprise getEntrepriseById(SessionFactory factory, int id) {
+        Session session = factory.getCurrentSession();
+
+        try {
+            // Start a transaction
+            session.beginTransaction();
+
+            // Retrieve the Entreprise by ID
+            Entreprise entreprise = session.get(Entreprise.class, id);
+
+            // Commit the transaction
+            session.getTransaction().commit();
+
+            return entreprise;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
